1dd1e27c00a8aaa24ca5e25744aa2186d1d4f3ce
qrow <mill7079@umn.edu>
2021-06-22 13:55:03 -0500

    layout edits - added button to go back to index page, added through script statement   at bottom of pages - just slap that line on the end of each recipe   after the ending html tag - changed stylesheet to allow for multiple background classes - trans   flag gradient now appears with class="trans"

---
+++ shared/footer.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+
+document.write("<footer class=\"back-buttton\">");
+document.write("<a href=\"..\/index.html\"><button>Back to Recipes<\/button><\/a>");
+document.write("<\/footer>");
